5ea52e83a68593d2f8763ff51cef597a84fb112b
AmoghKUrs25 <vvce23cse0157@vvce.acin>
2025-11-25 02:02:19 +0530

    Removed getPatients in hmsApplication and corrected path name of PatientController

--- src/main/java/com/example/hms/app/HmsApplication.java
+++ src/main/java/com/example/hms/app/HmsApplication.java
@@ -1,28 +1,12 @@
 package com.example.hms.app;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
-@RestController
 public class HmsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HmsApplication.class, args);
-    }
-
-    @GetMapping("/patients")
-    public List<Map<String, Object>> getPatients() {
-        return Arrays.asList(
-                Map.of("id", 1, "name", "Amogh", "age", 22, "disease", "Fever"),
-                Map.of("id", 2, "name", "Riya", "age", 30, "disease", "Flu")
-        );
     }
 }
--- src/main/java/com/example/hms/app/controller/PatientController.java
+++ src/main/java/com/example/hms/app/controller/PatientController.java
@@ -1,4 +1,4 @@
-package com.example.hms.controller;
+package com.example.hms.app.controller;
 
 import org.springframework.web.bind.annotation.*;
 
